977e6e000558a77bdc576b5207b5ca5d115d7b0a
ojitos369 <ojitos369@gmail.com>
2022-01-28 08:51:15 -0600

    parametres

--- hello_world/main.py
+++ hello_world/main.py
@@ -8,5 +8,24 @@ app = FastAPI()
 def hello_world():
     return {'message': 'Hello World'}
 
+@app.get('/tweets/{tweet_id}')
+def get_tweet(tweet_id='unknown_id_value'):
+    data = {
+        "tweet_id": tweet_id,
+        "content": f"This is data from tweet {tweet_id}"
+    }
+    return data
+
+@app.put('/user/{user_id}/details?name=no_dado&age=no_dado')
+def user_data(user_id, name='no_dado', age='no_dado'):
+    data = {
+        "user_id": user_id
+    }
+    if name != 'no_dado':
+        data["name"] = name
+    if age != 'no_dado':
+        data["age"] = age
+    return data
+
 # Run the application
 # $ uvicorn main:app --reload
