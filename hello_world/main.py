a20fdb5968126bea21ba402d7b275cf11088872d
ojitos369 <ojitos369@gmail.com>
2022-01-28 17:34:45 -0600

    validations_query

--- hello_world/main.py
+++ hello_world/main.py
@@ -7,6 +7,7 @@ from pydantic import BaseModel
 #FastAPI
 from fastapi import FastAPI
 from fastapi import Body
+from fastapi import Query
 
 app = FastAPI()
 
@@ -26,6 +27,30 @@ def hello_world():
 @app.post('/person/new')
 def create_person(person: Person = Body(...)): # '...' indicates that a parameter is obligatory
     return person
+
+# Validaciones: Query params
+@app.get('/person/detail')
+def show_person(
+    name: Optional[str] = Query(None, 
+    min_length = 1, max_length = 50),
+    age: str = Query(...)):
+    # Mala practica que un query param sea obligatorio
+    # Es recomenable que un path param si es obligatorio
+    return {'name': name, 'age': age}
+
+
+
+# max_length = max length of the string
+# min_length = min length of the string
+# regex = regular expression to validate the string
+# ge = greater or equal than
+# le = less or equal than
+# gt = greater than
+# lt = less than
+# title = title of the parameter
+# description = description of the parameter
+
+
 
 # Run the application
 # $ uvicorn main:app --reload
